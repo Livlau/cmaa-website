3889c0eea37d1fbc87ec6e65c8555b9953149584
LivL1004 <77578448+Livlau@users.noreply.github.com>
2023-02-21 03:02:24 +0000

    added programs and services

--- app.py
+++ app.py
@@ -10,9 +10,21 @@ def hello_cmaa():
   jobs = jobs_from_db()
   return render_template("home.html", jobs=jobs)
 
+
 @app.route("/about")
 def about_us():
   return render_template("about.html")
+
+
+@app.route("/programs")
+def programs_services():
+  return render_template("programs.html")
+
+
+@app.route("/civic-engagement")
+def civic_engagement():
+  return render_template("civic-engagement.html")
+
 
 @app.route("/api/jobs")
 def jobs_list():
